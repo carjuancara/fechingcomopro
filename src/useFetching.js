4fd9adbff82a8714f7bfbf40e3aa82e47c7f5410
Carlos Juan Carabajal <carjuancara@gmail.com>
2023-12-22 10:06:54 -0300

    feat: :sparkles: agrego la logica para hacer un estado de loading

--- src/useFetching.js
+++ src/useFetching.js
@@ -2,12 +2,15 @@ import { useState, useEffect } from 'react'
 
 export function useFeching(url) {
     const [data, setData] = useState(null)
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
+        setLoading(true)
         fetch(url)
             .then(response => response.json())
             .then(users => setData(users))
+            .finally(() => setLoading(false))
 
     }, [url])
-    return { data }
+    return { data, loading }
 }
