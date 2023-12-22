b8d8ff1d00f52d536fd003639ad4a47d0c4535d8
Carlos Juan Carabajal <carjuancara@gmail.com>
2023-12-22 10:13:51 -0300

    feat: :sparkles: agregue manejo de errores en el feching de datos

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import './App.css'
 import { useFeching } from './useFetching'
 
 function App() {
-  const { data, loading } = useFeching('https://jsonplaceholder.typicode.com/users')  
+  const { data, loading, error } = useFeching('https://jsonplaceholder.typicode.com/users')  
   return (
     <div className='App'>
       <h1>Feching como Pro</h1>
       <div className='card'>
       <ul>
+        {error && <li>Error: {error}</li>}
         {loading && <li>loading ...</li>}
         {data?.map( user => (
           <li key={user.id}>{user.name}</li>
--- src/useFetching.js
+++ src/useFetching.js
@@ -3,14 +3,15 @@ import { useState, useEffect } from 'react'
 export function useFeching(url) {
     const [data, setData] = useState(null)
     const [loading, setLoading] = useState(true)
-
+    const [error, setError] = useState(null)
     useEffect(() => {
         setLoading(true)
         fetch(url)
             .then(response => response.json())
             .then(users => setData(users))
+            .catch(err => setError(err))
             .finally(() => setLoading(false))
 
     }, [url])
-    return { data, loading }
+    return { data, loading, error }
 }
