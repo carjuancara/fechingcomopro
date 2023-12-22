bf62773550f2709e0519ffde7e1e87f478433228
Carlos Juan Carabajal <carjuancara@gmail.com>
2023-12-22 10:05:49 -0300

    feat: :sparkles: agrego un mensaje de loading hasta que lleguen los datos

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import './App.css'
 import { useFeching } from './useFetching'
 
 function App() {
-  const { data } = useFeching('https://jsonplaceholder.typicode.com/users')  
+  const { data, loading } = useFeching('https://jsonplaceholder.typicode.com/users')  
   return (
     <div className='App'>
       <h1>Feching como Pro</h1>
       <div className='card'>
       <ul>
+        {loading && <li>loading ...</li>}
         {data?.map( user => (
           <li key={user.id}>{user.name}</li>
         ))}
